5150fb22bd472ca15c712a6d22202e497dc4f2a5
Nagy Gergő <nagygergo1990@gmail.com>
2025-06-15 11:15:34 +0200

    cart

--- frontend/src/app/components/home/cart/cart.component.ts
+++ frontend/src/app/components/home/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal, WritableSignal } from '@angular/core';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { CartItem } from '../types/cart.type';
 import { Router } from '@angular/router';
@@ -8,10 +8,23 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
 import { RatingsComponent } from '../../ratings/ratings.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoggedInUser } from '../types/user.type';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-cart',
-  imports: [FontAwesomeModule, CommonModule, RatingsComponent],
+  imports: [
+    FontAwesomeModule,
+    CommonModule,
+    RatingsComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
@@ -20,7 +33,42 @@ export class CartComponent {
   faBoxOpen = faBoxOpen;
   faShoppingCart = faShoppingCart;
 
-  constructor(public cartStore: CartStoreItem, private router: Router) {}
+  user = signal<LoggedInUser>({
+    firstName: '',
+    lastName: '',
+    address: '',
+    city: '',
+  });
+
+  orderForm: WritableSignal<FormGroup>;
+
+  constructor(
+    public cartStore: CartStoreItem,
+    private router: Router,
+    private userService: UserService,
+    private fb: FormBuilder
+  ) {
+    this.orderForm = signal(this.createOrderForm(this.user()));
+    this.userService.loggedInUser$.subscribe((u) => this.user.set(u));
+
+    effect(() => {
+      const newUser = this.user();
+      this.orderForm.set(this.createOrderForm(newUser));
+    });
+  }
+
+  private createOrderForm(user: LoggedInUser | null): FormGroup {
+    return this.fb.group({
+      name: [
+        user?.firstName && user?.lastName
+          ? `${user.firstName} ${user.lastName}`.trim()
+          : '',
+        Validators.required,
+      ],
+      address: [user?.address || '', Validators.required],
+      city: [user?.city || '', Validators.required],
+    });
+  }
 
   navigateToHome(): void {
     this.router.navigate(['home/products']);
@@ -36,5 +84,13 @@ export class CartComponent {
 
   removeItem(cartItem: CartItem): void {
     this.cartStore.removeProduct(cartItem);
+  }
+
+  onSubmit(): void {
+    if (this.orderForm().valid) {
+      console.log('Order submitted: ', this.orderForm().value);
+    } else {
+      console.log('Invalid order form');
+    }
   }
 }
